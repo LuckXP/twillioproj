b3cb237683dcf3d80af8216a4dd23a228c3e246b
LuckXP <js.sindler@gmail.com>
2016-04-04 16:28:46 -0600

    stuff like npms and stuff and like a bunch of other stuff

---
+++ config.js
@@ -0,0 +1,15 @@
+var cfg = {};
+cfg.twilio = {
+  accountSid: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
+  from: process.env.TWILIO_FROM
+};
+
+cfg.myPhone = process.env.MY_PHONE;
+
+cfg.hostname = 'localhost';
+cfg.port = 3000;
+
+
+
+module.exports = cfg;
